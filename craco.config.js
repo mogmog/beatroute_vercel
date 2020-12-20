fec0a464d0dab19f5eb96ef6d375a6444848fb97
graham_bates <graham.bates@spglobal.com>
2020-12-20 14:47:21 +0000

    hhhh

---
+++ craco.config.js
@@ -0,0 +1,28 @@
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const WebpackBar = require("webpackbar");
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const CracoAntDesignPlugin = require("craco-antd");
+const path = require("path");
+
+module.exports = {
+
+  plugins: [
+
+
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        customizeThemeLessPath: path.join(
+            __dirname,
+            "src/customTheme.less"
+        )
+      }
+    }
+  ],
+
+  webpack: {
+    headers: {
+    }
+  }
+};
